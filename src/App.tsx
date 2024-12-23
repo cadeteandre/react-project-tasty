2c0ccfe8f82738145c5bf30914824356cae638c0
Minyeong Jeong <megormin@gmail.com>
2024-12-23 16:34:08 +0100

    page Home done

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import './App.css'
-import RootLayout from './rootLayout/RootLayout'
-import Home from './pages/home/Home'
-import FilteredByCat from './pages/filteredByCat/FilteredByCat'
-import RecipePage from './pages/recipePage/RecipePage'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./App.css";
+import RootLayout from "./rootLayout/RootLayout";
+import Home from "./pages/home/Home";
+import FilteredByCat from "./pages/filteredByCat/FilteredByCat";
+import RecipePage from "./pages/recipePage/RecipePage";
 
 function App() {
-
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path='/' element={<RootLayout />}>
+      <Route path="/" element={<RootLayout />}>
         <Route index element={<Home />} />
-        <Route path='/categories' element={<FilteredByCat />} />
-        <Route path='/recipe/:mealID' element={<RecipePage />} />
+        <Route path="/categories/:categoryName" element={<FilteredByCat />} />
+        <Route path="/recipe/:mealID" element={<RecipePage />} />
       </Route>
     )
-  )
+  );
 
   return (
     <>
       <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
