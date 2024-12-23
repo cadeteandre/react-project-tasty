7c57bc0abe3aa26b1f6f9eddbaa287a793811b0a
Andre Cadete <andrecadete70@gmail.com>
2024-12-23 11:31:55 +0100

    interfaces created

---
+++ src/interfaces/IFilteredCat.ts
@@ -0,0 +1,9 @@
+export interface IFilteredCat {
+    meals: IFilteredMeal[];
+}
+
+export interface IFilteredMeal {
+    strMeal:      string;
+    strMealThumb: string;
+    idMeal:       string;
+}
