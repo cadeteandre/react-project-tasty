c13e4ac5dbac3c8ced90adb0765c27fd7b9bf668
Sharon <shar1.v.kori@gmail.com>
2024-12-23 13:20:27 +0100

    added html and css for both cards

--- src/components/categoryCard/CategoryCard.tsx
+++ src/components/categoryCard/CategoryCard.tsx
@@ -2,7 +2,13 @@ import "./CategoryCard.css";
 
 const CategoryCard = () => {
     return ( 
+        <section className="category-card__wrapper">
         <h1>Category Card</h1>
+        <article className="category-card">
+            <p>category{}</p>
+            <img src="" alt="meal img" />
+        </article>
+        </section>
     );
 }
 
--- src/pages/filteredByCat/FilteredByCat.tsx
+++ src/pages/filteredByCat/FilteredByCat.tsx
@@ -1,8 +1,12 @@
+import FilteredCard from "../../components/filteredCard/FilteredCard";
 import "./FilteredByCat.css";
 
 const FilteredByCat = () => {
     return (  
+       <>
         <h1>Filtered by Categories</h1>
+        <FilteredCard/>
+        </>
     );
 }
 
