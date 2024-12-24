fda203da70019e9028cda4ee9e7cac1c85dbeef7
Sharon <shar1.v.kori@gmail.com>
2024-12-24 09:16:38 +0100

    added filtered cat and card

--- src/pages/filteredByCat/FilteredByCat.tsx
+++ src/pages/filteredByCat/FilteredByCat.tsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
 import FilteredCard from "../../components/filteredCard/FilteredCard";
 import "./FilteredByCat.css";
+import { IFilteredCat, IFilteredMeal } from '../../interfaces/IFilteredCat';
+
 
 const FilteredByCat = () => {
+
+    const [filteredCatData, setFilteredCatData] = useState<IFilteredCat[]>([]);
+
+    useEffect(() => {
+        fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=beef')
+        .then((res) => res.json())
+        .then((data) => setFilteredCatData(data.meals))
+    }, []);
+    console.log('data logged', filteredCatData);
+    
+
     return (  
        <>
-        <h1>Filtered by Categories</h1>
-        <FilteredCard/>
+        <h1>Everything categoryname</h1>
+        <section className="filtered-card__wrapper">
+            {filteredCatData.map((filteredMeals) => (
+            <FilteredCard key={filteredMeals.meals} filteredMeals={filteredMeals}/>
+        ))}
+        </section>
         </>
     );
 }
